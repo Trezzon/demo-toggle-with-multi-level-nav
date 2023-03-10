289fe6595b852e14a21aa87ba47102248625d12e
Terrence Cabillo <tcabillo1@nait.ca>
2023-03-10 11:47:37 -0700

    confinv2ini

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 // js file
+
+console.log("connected");
+
+const btn = document.querySelector(".toggle-btn");
+
+btn.addEventListener("click", () => {
+    document.querySelector("nav").classList.toggle("show-nav");
+});
